54d8989152302cca591efd35f35955dc3c3adb0c
pardnchiu <dev@pardn.io>
2026-01-07 14:37:31 +0800

    feat: set notify

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
+)
+
+const (
+	notifyKey = "ticker_notify"
+)
+
+var (
+	DB *sql.DB
 )
 
 type db struct {
@@ -36,11 +45,11 @@ func main() {
 		dbName:   "database",
 		sslMode:  "disable",
 	}
-
-	db, err := sql.Open("postgres", fmt.Sprintf(
+	link := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		config.host, config.port, config.user, config.password, config.dbName, config.sslMode,
-	))
+	)
+	db, err := sql.Open("postgres", link)
 	if err != nil {
 		slog.Error("open db failed", "error", err)
 		os.Exit(1)
@@ -52,9 +61,51 @@ func main() {
 		os.Exit(1)
 	}
 
+	DB = db
+
 	slog.Info("database connected",
 		"host", config.host,
 		"port", config.port,
 		"database", config.dbName,
 	)
+
+	listener := pq.NewListener(
+		link,
+		10*time.Second,
+		time.Minute,
+		func(ev pq.ListenerEventType, err error) {
+			switch ev {
+			case pq.ListenerEventConnected:
+				slog.Info("listener connected")
+			case pq.ListenerEventDisconnected:
+				slog.Warn("listener disconnected", "error", err)
+			case pq.ListenerEventReconnected:
+				slog.Info("listener reconnected")
+			case pq.ListenerEventConnectionAttemptFailed:
+				slog.Error("connection attempt failed", "error", err)
+			}
+		})
+	defer listener.Close()
+
+	if err := listener.Listen(notifyKey); err != nil {
+		slog.Error("listen failed", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("listening", "channel", notifyKey)
+
+	for {
+		select {
+		case <-ctx.Done():
+			os.Exit(1)
+		case notification := <-listener.Notify:
+			if notification == nil {
+				os.Exit(1)
+			}
+		case <-time.After(30 * time.Second):
+			if err := listener.Ping(); err != nil {
+				slog.Error("ping failed", "error", err)
+			}
+		}
+	}
 }
